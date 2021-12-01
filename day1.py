ed46ab7b0984e405cc696de7150408ba54e83b29
Kristi Achilleos <krispy2009@gmail.com>
2021-12-01 07:51:51 +0200

    day 1 part 2

--- day1.py
+++ day1.py
@@ -7,3 +7,21 @@ with open("input.txt") as f:
                 count += 1
         prev = int(line)
 print(count)
+
+prev = None
+count = 0
+data = []
+with open("input.txt") as f:
+    for line in f.readlines():
+        data.append(int(line))
+group = [0, 0, 0]
+for idx, i in enumerate(data):
+    if idx <= len(data) - 3:
+        group[0] = i
+        group[1] = data[idx + 1]
+        group[2] = data[idx + 2]
+        if prev and sum(group) > prev:
+            # print(group, " = ", sum(group), f" (increase from {prev})")
+            count += 1
+    prev = sum(group)
+print(count)
