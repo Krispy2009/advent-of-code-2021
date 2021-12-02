1deb0bdade35374da3b27e458e89ebc43ac6afce
Kristi Achilleos <krispy2009@gmail.com>
2021-12-02 12:44:26 +0200

    day 2 part 2

--- day2/day2.py
+++ day2/day2.py
@@ -12,3 +12,17 @@ with open("input.txt") as f:
 
     print(f"horizontal: {horizontal} x depth: {depth} = {horizontal*depth}")
 
+horizontal, depth, aim = 0, 0, 0
+with open("input.txt") as f:
+    for line in f.readlines():
+        direction, distance = line.split(" ")
+        distance = int(distance)
+        if direction == "forward":
+            horizontal += distance
+            depth += aim * distance
+        elif direction == "down":
+            aim += distance
+        elif direction == "up":
+            aim -= distance
+
+    print(f"horizontal: {horizontal} x depth: {depth} = {horizontal*depth}")
