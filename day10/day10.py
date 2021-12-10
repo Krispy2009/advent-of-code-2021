5698b889c91e6862e35978c9b1c7be065f7490a5
Kristi Achilleos <Krispy2009@gmail.com>
2021-12-10 17:42:24 +0200

    day 10 part 2

--- day10/day10.py
+++ day10/day10.py
@@ -1,7 +1,6 @@
 
 def check_line(line):
     open_chars = ['{','[','(', '<']
-    closed_chars = ['}',']',')', '>']
     bracket_pairs= {'{': '}', '[':']','(':')', '<':'>'}
     matching_pairs = []
     for ch in line:
@@ -32,5 +31,47 @@ def part1():
         total_score += invalid_chars[c] * scores[c]
     
     print(total_score)
+
+def complete_line(line):
+    open_chars = ['{','[','(', '<']
+    bracket_pairs= {'{': '}', '[':']','(':')', '<':'>'}
+    matching_pairs = []
+
+    for ch in line:
+        if ch in open_chars:
+            matching_pairs.append(ch)
+
+        else:
+            if ch == bracket_pairs[matching_pairs.pop()]:
+                continue
+    return ''.join([bracket_pairs[c] for c in matching_pairs[::-1]])
+
+def score_line(ending):
+    total_score = 0
+    scores = {'}':3,']':2,')':1, '>':4}
+    for c in ending:
+        total_score *= 5
+        total_score += scores[c]
+    return total_score
+
+
+
+def part2():
+    all_scores = []
+    valid_lines = []
+    with open('input.txt') as f:
+        for line in f.readlines():
+            total_score = 0
+            line = line.strip()
+            char = check_line(line)
+            if char is None:
+                valid_lines.append(line)
+                print(f'{line} is valid')
+                ending = complete_line(line)
+                print(f'{line} complete by adding {ending}')
+                all_scores.append(score_line(ending))
+    # for c in invalid_chars:
+    #     total_score += invalid_chars[c] * scores[c]
+    print(sorted(all_scores)[int(len(all_scores)/2)])
 if __name__ == "__main__":
-    part1()
+    part2()
