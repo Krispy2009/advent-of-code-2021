e8f117b16aa325e5bc3015bea262b4c07b0d5354
Kristi Achilleos <Krispy2009@gmail.com>
2021-12-17 15:06:42 +0200

    day 17 part 2

--- day17/day17.py
+++ day17/day17.py
@@ -25,44 +25,32 @@ def shoot_probe(x_vel, y_vel, target_X, target_Y):
         x, y, x_vel, y_vel = step(x,y,x_vel, y_vel)
         path.append((x,y))
         if target_X[0] <= x <= target_X[1] and target_Y[1] <= y <= target_Y[0]:
-            break 
+            return path 
         if y < target_Y[1]:
             break
-
-    for (a,b) in path:
-        if target_Y[1] <= b <= target_Y[0] and target_X[0] <= a <= target_X[1]:
-            return path
 
 
 
 def part1():
-    highest_points = {}
+    highest_points = []
+    for x_vel in range(x2+1):
+        for y_vel in range(y2, -y1):
+            path = shoot_probe(x_vel, y_vel, (x1,x2), (y2,y1))
+            if path:
+                highest_point = sorted(path, key=lambda x: x[1], reverse=True)[0][1]
+                highest_points.append(highest_point)
+    print(f"Part1: {max(highest_points)}")
+
+def part2():
+    velocities = []
     for x_vel in range(x2+1):
         for y_vel in range(y1, -y1+1):
             path = shoot_probe(x_vel, y_vel, (x1,x2), (y2,y1))
             if path:
-                highest_point = sorted(path, key=lambda x: x[1], reverse=True)[0][1]
-                highest_points[highest_point] = (x_vel, y_vel)
-                print(f'found a path to the target with initial vel: {(x_vel, y_vel)} reaching {highest_point}')
-                print(max(highest_points))
-    return max(highest_points)
-
-
-
-# def part1():
-#     x,y = 0,0
-#     points = []
-#     for x_vel in range(-x2, x2):
-#         for y_vel in range(y1, -y1):
-#             for _ in range(10):
-#                 x, y, x_vel, y_vel = step(x,y,x_vel,y_vel)
-#                 print((x,y), f'xV {x_vel}, yV {y_vel}') 
-#                 points.append((x,y))
-#                 if is_after_target(x,y):
-#                     print(points)
-#                     import pdb; pdb.set_trace()
-
+                velocities.append((x_vel, y_vel))
+    print(f"Part2: {len(velocities)}")
     
 
 if __name__ == "__main__":
     part1()
+    part2()
