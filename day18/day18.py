bdb6206f6e90bd8d04196098d76916dd272fd4da
Kristi Achilleos <Krispy2009@gmail.com>
2021-12-22 21:30:15 +0200

    day18 part 1 🤯

--- day18/day18.py
+++ day18/day18.py
@@ -3,11 +3,10 @@ import math
 example = [[1,1], [2,2], [3,3], [4,4]]
 homework = []
 def part1():
-    with open('small-example.txt') as f: 
+    with open('input.txt') as f: 
         for line in f.readlines():
             homework.append(eval(line.strip()))
     print (len(homework)) 
-    pdb.set_trace()
     sn = SnailfishNumber(homework[0], 0, None)
     for line in homework[1:]:
         new_sn = SnailfishNumber(line, 0, None)
@@ -17,11 +16,22 @@ def part1():
         print(f"= {sn}")
         print()
 
+    print(f"Magnitude: {magnitude(sn)}")
+
 
 
 class Node():
+
+    def __truediv__(self, a):
+        self.value = self.value / a
+        return self
+
+    def __ge__(self, a):
+        return self.value >= a
+
     def __add__(self, a):
-        return self.value + a.value
+        self.value = self.value + a.value
+        return self
 
     def __str__(self):
         return f"{self.value}"
@@ -38,7 +48,7 @@ class Node():
             print(f'Node attribute says no: {item}')
 
 
-    def __init__(self, value,depth,  parent):
+    def __init__(self, value, depth,  parent):
         self.value = value
         self.parent = parent
         self.right = None
@@ -47,6 +57,19 @@ class Node():
 
     def increase_depth(self):
         self.depth +=1
+
+    def should_split(self):
+        return self.value >=10 
+    
+    def split(self):
+        l = math.floor(self.value/2)
+        r = math.ceil(self.value/2)
+
+        l = Node(l, self.parent.depth+1, self.parent)
+        r = Node(r, self.parent.depth+1, self.parent)
+
+        return SnailfishNumber([l, r], self.depth, self)
+
 class SnailfishNumber():
     def __repr__(self):
         return self.__str__()
@@ -70,11 +93,19 @@ class SnailfishNumber():
 
         if isinstance(pair[0], (list, SnailfishNumber)):
             self.left = SnailfishNumber(pair[0], self.depth+1, self)
-        elif isinstance(pair[0], (int, Node)):
+        elif isinstance(pair[0], (int)):
             self.left = Node(pair[0],self.depth+1, self)
+        elif isinstance(pair[0], (Node)):
+            self.left = pair[0]
+            self.left.depth = self.depth +1
+            self.left.parent = self
         if isinstance(pair[1], (list, SnailfishNumber)):
             self.right = SnailfishNumber(pair[1], self.depth+1, self)
-        elif isinstance(pair[1], (int,Node)):
+        elif isinstance(pair[1], (Node)):
+            self.right = pair[1]
+            self.right.depth = self.depth +1
+            self.right.parent = self
+        elif isinstance(pair[1], (int)):
             self.right = Node(pair[1], self.depth+1,self)
     
     def increase_depth(self):
@@ -87,7 +118,6 @@ class SnailfishNumber():
 
 
     def explode(self):
-        pdb.set_trace()
         leftmost, rightmost = None, None
         node = self.find_node_to_explode()
         
@@ -103,9 +133,9 @@ class SnailfishNumber():
         parent = node.parent
         if parent is not None:
             if parent.left == node:
-                parent.left = 0
+                parent.left = Node(0, parent.depth + 1, parent)
             elif parent.right == node:
-                parent.right = 0
+                parent.right = Node(0, parent.depth + 1, parent)
         
 
     def should_explode(self):
@@ -127,7 +157,6 @@ class SnailfishNumber():
         return node
 
     def find_closest_right(self):
-        pdb.set_trace()
         if self.parent is None: 
             return None
         if self == self.parent.right:
@@ -143,12 +172,22 @@ class SnailfishNumber():
             node = self.find_node_to_explode()
             if node and node.should_explode():
                 node.explode()
+                print('After explode: ', self)
                 continue
 
             node = self.find_node_to_split()
-
             if node and node.should_split():
-                node.split()
+                new_node = node.split()
+                new_node.parent = node.parent
+
+                if node.parent.left == node:
+
+                    node.parent.left = new_node
+                else:
+                    node.parent.right = new_node
+                
+                print('After split: ', self)
+
                 continue
             if not self.should_explode() and not self.should_split():
                 break
@@ -156,50 +195,53 @@ class SnailfishNumber():
 
 
     def find_node_to_explode(self):
+        l, r = None, None
         if self.depth >= 4:
             return self
 
-        if not isinstance(self.left, Node):
-            return self.left.find_node_to_explode()
-        if not isinstance(self.right, Node):
-            return self.right.find_node_to_explode()
+        if not isinstance(self.left, (int, Node)):
+             l = self.left.find_node_to_explode()
 
-        return None
+        if not isinstance(self.right, (int, Node)):
+             r = self.right.find_node_to_explode()
+
+        return l or r
             
 
     def find_node_to_split(self):
-        
-        if isinstance(self.left, int) and self.left >=10:
-            return self
+        l, r = None, None
+        if isinstance(self.left, Node) and self.left.value >=10:
+            l = self.left
         elif isinstance(self.left, SnailfishNumber):
-            return self.left.find_node_to_split()
+            l = self.left.find_node_to_split()
 
-        if isinstance(self.right, int) and self.right >=10:
-            return self
+        if isinstance(self.right, Node) and self.right.value >=10:
+            r =  self.right
         elif isinstance(self.right, SnailfishNumber):
-            return self.right.find_node_to_split()
+            r =  self.right.find_node_to_split()
         
-        return None
+        return l or r
 
 
 
     def should_split(self):
-        if isinstance(self.left, int) and self.left >=10:
+        if isinstance(self.left, Node) and self.left.value >=10:
             return True
-        elif isinstance(self.right, int) and self.right >=10:
+        if isinstance(self.right, Node) and self.right.value >=10:
             return True
         else:
             return False
+
     
-    def split(self):
-        if isinstance(self.left, int) and self.left >=10:
-            l = math.floor(self.left/2)
-            r = math.ceil(self.left/2)
-            self.left = SnailfishNumber([l, r], self.depth, self)
+def magnitude(n):
+    if isinstance(n, Node):
+        return n.value
+    else:
+        return 3 * magnitude(n[0]) + 2 * magnitude(n[1])
 
 
 def add(num1, num2):
-    new_num = SnailfishNumber([num1, num2], 0, None)
+    new_num = SnailfishNumber([num1, num2], -1, None)
     
     num1.parent = new_num
     num2.parent = new_num
